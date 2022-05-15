c05f76d1990885e98c0f689b5516ce6a22783d7f
phamxuancan <kudo2616@gmail.com>
2022-05-15 21:41:17 +0700

    init

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $success = true;
+            $message = 'User register successfully';
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+
+        // response
+        $response = [
+            'success' => $success,
+            'message' => $message,
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credentials, true)) {
+            $success = true;
+            $message = 'User login successfully';
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error in Login');
+            }
+            $accessToken = $user->createToken('authToken')->plainTextToken;
+        } else {
+            $success = false;
+            $message = 'Unauthorised';
+            $accessToken = '';
+        }
+
+        // response
+        $response = [
+            'success' => $success,
+            'message' => $message,
+            'accessToken' => $accessToken,
+            
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Logout
+     */
+    public function logout(Request $request)
+    {
+        try {
+            if(method_exists(auth()->user()->currentAccessToken(), 'delete')) {
+                auth()->user()->currentAccessToken()->delete();
+            }
+            
+            auth()->guard('web')->logout();
+            Session::flush();
+            $success = true;
+            $message = 'Successfully logged out';
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+
+        // response
+        $response = [
+            'success' => $success,
+            'message' => $message,
+        ];
+        return response()->json($response);
+    }
+}
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        Welcome {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard",
+    data() {
+        return {
+            name: null,
+        }
+    },
+    created() {
+        console.log(window.Laravel);
+        if (window.Laravel.user) {
+            this.name = window.Laravel.user.name
+        }
+    },
+    // beforeRouteEnter(to, from, next) {
+    //     if (!window.Laravel.isLoggedin) {
+    //         window.location.href = "/";
+    //     }
+    //     next();
+    // }
+}
+</script>
