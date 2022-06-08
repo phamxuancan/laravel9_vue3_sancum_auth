ac3dfc7662909e6a43944730b22c26e5d2540b81
phamxuancan <kudo2616@gmail.com>
2022-06-08 21:19:07 +0700

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
-use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Session;
+use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
